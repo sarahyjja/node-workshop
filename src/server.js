97ab0144392388d3945894da245b6ea1f1a1df60
sarah <sarahkharraz@gmail.com>
2019-07-22 15:04:01 +0100

    Communicate with the server Create handler function Give order to the function Pass the function to server Run the server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
 const http = require('http')
-const server = http.createServer()
+const server = http.createServer(handler)
 const port = 3000
+
+const message = 'Workshop'
+
+function handler(request, response){
+  response.writeHead(200, {'content-type': 'text/html'})
+  response.write(message)
+  response.end()
+}
 
 server.listen(3000, function(){
   console.log(`The server is listening on the port ${port}`)
